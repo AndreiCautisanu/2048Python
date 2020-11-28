fb65e1bc95cf133770f7aaf127581e6606c21e4a
Andrei Cautisanu <andrei.cautisanu@gmail.com>
2020-11-28 20:28:41 +0200

    Added merge function
    Function that merges tiles with the same number after sliding left. Similar to the slide function, we can just use this one function on a rotated board instead of writing four of them

--- 2048.py
+++ 2048.py
@@ -23,8 +23,9 @@ def init_board():
 
 
 # function that slides all tiles to the left
-# we only need to implement this one function instead of one for each direction, thanks to the fact that we can move in all other directions
-# by flipping the board matrix around in different ways in our move methods
+# we only need to implement this one function instead of one for each direction, due to the fact that we can move in all other directions
+# by rotating the board matrix around in our move methods and sliding to the left
+# e.g. moving up is just rotating the board counter-clockwise, sliding the tiles to the left, and rotating the board matrix back
 
 def slide_left(board):
 
@@ -44,6 +45,29 @@ def slide_left(board):
 
 
 
+# same rationale applies to the merging function, which will only merge equal numbered tiles as if we had just moved left
+# we can apply the merge function on the rotated board matrix when we want to move up, down or right, as i've explained before the slide_left function
+
+def merge(board):
+
+    new_board = board
+
+    for row_i in range(4):
+        for col_i in range(3):
+            if new_board[row_i][col_i] == new_board[row_i][col_i + 1] and new_board[row_i][col_i] != 0:
+                new_board[row_i][col_i] *= 2
+                new_board[row_i][col_i + 1] = 0
+
+    return slide_left(new_board)  # slide again to cover empty spaces
+
+
+
+
+
+
 if __name__ == "__main__":
-    board = np.array([[0, 2, 2, 0], [0, 0, 0, 4], [0, 2, 4, 4], [0, 4, 0, 2]])
-    print(slide_left(board))
+    board = np.array([[2, 2, 4, 0], [8, 8, 0, 2], [2, 4, 0, 4], [0, 4, 0, 2]])
+    board = slide_left(board)
+    print(board)
+    board = merge(board)
+    print(board)
