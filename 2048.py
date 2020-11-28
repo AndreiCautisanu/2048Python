2a2032c57bd6386c482a9f78c507221052adec79
Andrei Cautisanu <andrei.cautisanu@gmail.com>
2020-11-28 19:03:47 +0200

    Added slide function
    Function that performs the sliding of the tiles to the left. Only need one function because we can do the other moves by rotating the board matrix and sliding left on it

--- 2048.py
+++ 2048.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 
-#function that initializes and returns the starting board
-def init():
+# function that initializes and returns the starting board
+def init_board():
     board = np.zeros((4,4), dtype=int)
 
     rng = np.random.default_rng()
@@ -16,5 +16,34 @@ def init():
 
     print(board)
 
+    return board
+
+
+
+
+
+# function that slides all tiles to the left
+# we only need to implement this one function instead of one for each direction, thanks to the fact that we can move in all other directions
+# by flipping the board matrix around in different ways in our move methods
+
+def slide_left(board):
+
+    new_board = np.zeros((4,4), dtype=int)
+    i = 0
+
+    for row in board:
+        arr = list(row[row != 0])                           # get tiles in row that are not empty
+        empty_spaces_no = 4 - len(arr)                      # create an array of zeros equal to the number of empty spaces on the row
+        zeros = [0 for i in range(empty_spaces_no)]
+        new_row = arr + zeros                               # concatenate the two arrays
+        new_board[i] = new_row
+        i += 1
+
+    
+    return new_board
+
+
+
 if __name__ == "__main__":
-    init()
+    board = np.array([[0, 2, 2, 0], [0, 0, 0, 4], [0, 2, 4, 4], [0, 4, 0, 2]])
+    print(slide_left(board))
