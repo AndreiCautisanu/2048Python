18f4d0d3ec97b28bd2d5775742ad43ae9212fdbb
Andrei Cautisanu <andrei.cautisanu@gmail.com>
2020-11-29 22:34:01 +0200

    Added confirmation flags to slide, merge and move functions
    Functions now return flags to confirm whether the attempted move was successful, commiting this before attempting to remove the new variables from the move functions and just doing it with board

--- 2048.py
+++ 2048.py
@@ -30,6 +30,7 @@ def init_board():
 def slide_left(board):
 
     new_board = np.zeros((4,4), dtype=int)
+    flag = False
     i = 0
 
     for row in board:
@@ -38,10 +39,17 @@ def slide_left(board):
         zeros = [0 for i in range(empty_spaces_no)]
         new_row = arr + zeros                               # concatenate the two arrays
         new_board[i] = new_row
+
+        compare = row == new_row
+        rows_equal = compare.all()
+
+        if not rows_equal:
+            flag = True                                     # new row different from original row, so slide was possible
+
         i += 1
 
     
-    return new_board
+    return (new_board, flag)
 
 
 
@@ -50,15 +58,19 @@ def slide_left(board):
 
 def merge(board):
 
-    new_board = board
+    new_board = np.copy(board)
+    flag = False
 
     for row_i in range(4):
         for col_i in range(3):
             if new_board[row_i][col_i] == new_board[row_i][col_i + 1] and new_board[row_i][col_i] != 0:
                 new_board[row_i][col_i] *= 2
                 new_board[row_i][col_i + 1] = 0
+                flag = True
 
-    return slide_left(new_board)  # slide again to cover empty spaces
+    slide_board, _ = slide_left(new_board)
+
+    return slide_board, flag  # slide again to cover empty spaces
 
 
 
@@ -88,36 +100,43 @@ def add_new_tile(board):
 
 # function for moving up, rotate board counter-clockwise once, slide and merge to the left, rotate it back
 def player_move_up(board):
-    board = np.rot90(board)
-    board = slide_left(board)
-    board = merge(board)
-    board = np.rot90(board, -1)
-    return board
+    rotated_board = np.rot90(board)
+    slide_board, flag_slide_success = slide_left(rotated_board)
+    merge_board, flag_merge_success = merge(slide_board)
+    flag_move_success = flag_slide_success or flag_merge_success
+
+    original_board = np.rot90(merge_board, -1)
+    return (original_board, flag_move_success)
 
 
 # function for moving down, rotate board clockwise once, slide and merge to the left, rotate it back
 def player_move_down(board):
-    board = np.rot90(board, -1)
-    board = slide_left(board)
-    board = merge(board)
-    board = np.rot90(board)
-    return board
+    rotated_board = np.rot90(board, -1)
+    slide_board, flag_slide_success = slide_left(rotated_board)
+    merge_board, flag_merge_success = merge(slide_board)
+    flag_move_success = flag_slide_success or flag_merge_success
+
+    original_board = np.rot90(merge_board)
+    return (original_board, flag_move_success)
 
 
 # function for moving left, no rotation necessary
 def player_move_left(board):
-    board = slide_left(board)
-    board = merge(board)
-    return board
+    slide_board, flag_slide_success = slide_left(board)
+    merge_board, flag_merge_success = merge(slide_board)
+    flag_move_success = flag_slide_success or flag_merge_success
+
+    return (merge_board, flag_move_success)
 
 
 # function for moving right, rotate board twice, slide and merge to the left, rotate it back
 def player_move_right(board):
-    board = np.rot90(board, 2)
-    board = slide_left(board)
-    board = merge(board)
-    board = np.rot90(board, -2)
-    return board
+    rotated_board = np.rot90(board, 2)
+    slide_board, flag_slide_success = slide_left(rotated_board)
+    merge_board, flag_merge_success = merge(slide_board)
+    flag_move_success = flag_slide_success or flag_merge_success
+    original_board = np.rot90(merge_board, -2)
+    return (original_board, flag_move_success)
 
 
 
@@ -136,12 +155,21 @@ if __name__ == "__main__":
 
     board = np.array([[2, 2, 4, 0], [2, 0, 4, 0], [0, 0, 2, 0], [0, 0, 2, 0]])
     print(board)
-    board = player_move_up(board)
+    board, flag = player_move_up(board)
     print(board)
 
     print("---")
 
     board = np.array([[2, 2, 0, 0], [4, 0, 0, 0], [8, 2, 0, 2], [8, 0, 0, 2]])
     print(board)
-    board = player_move_down(board)
+    board, flag = player_move_down(board)
     print(board)
+
+    print("---")
+
+    board = np.array([[2, 0, 0, 0], [2, 0, 0, 0], [2, 0, 0, 0], [2, 0, 0, 0]])
+    print(board)
+    board, flag = player_move_left(board)
+    print(board)
+    print(flag)
+
